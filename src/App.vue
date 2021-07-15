85b1d1e9f81c3334bf9ac9ebc012f4798117e185
wangyz <891634428@qq.com>
2021-07-15 22:12:27 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,11 @@
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  // components: {
-  //   HelloWorld
-  // }
+// import HelloWorld from './components/He
+}
+}
+}
+}
 }
 </script>
 
